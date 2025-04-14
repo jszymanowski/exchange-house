e14a1461955de7736da22e2c26cc4bee542fbf26
John Szymanowski <github@john.szymanow.ski>
2025-04-14 14:03:06 +0800

    remove unused schema

--- app/schema/simple_rate_response.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-from decimal import Decimal
-
-from pydantic import BaseModel, Field
-
-
-class SimpleRateResponse(BaseModel):
-    rate: Decimal = Field(gt=0)
-    date: date
