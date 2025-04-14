582d0001e830e99c5d8502aa53d3b9ac1b0dd2db
John Szymanowski <github@john.szymanow.ski>
2025-04-14 10:55:07 +0800

    Add a second test to demonstrate db is cleared between runs

--- tests/models/test_exchange_rate.py
+++ tests/models/test_exchange_rate.py
@@ -49,3 +49,27 @@ async def test_exchange_rate_model() -> None:
     assert committed_record.quote_currency_code == "EUR"
     assert committed_record.rate == Decimal("0.85")
     assert committed_record.data_source == "test"
+
+
+@pytest.mark.asyncio
+async def test_exchange_rate_model_another() -> None:
+    exchange_rate = ExchangeRate(
+        as_of=date(2021, 1, 1),
+        base_currency_code="USD",
+        quote_currency_code="EUR",
+        rate=Decimal("0.85"),
+        data_source="test",
+    )
+
+    await exchange_rate.save()
+
+    committed_record = await ExchangeRate.get(id=exchange_rate.id)
+
+    print(committed_record)
+
+    assert committed_record.id is not None
+    assert committed_record.as_of == date(2021, 1, 1)
+    assert committed_record.base_currency_code == "USD"
+    assert committed_record.quote_currency_code == "EUR"
+    assert committed_record.rate == Decimal("0.85")
+    assert committed_record.data_source == "test"
