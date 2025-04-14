6ecc9402aa4ba2ea51303363389b40f221df88b4
John Szymanowski <github@john.szymanow.ski>
2025-04-14 12:09:52 +0800

    Fix script

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -5,7 +5,7 @@ set -x
 
 source .env
 
-SELECT_QUERY="SELECT id, as_of, from_iso_code AS base_currency_code, to_iso_code AS quote_currency_code, rate, created_at, updated_at FROM exchange_rates;"
+SELECT_QUERY="SELECT id, as_of, from_iso_code AS base_currency_code, to_iso_code AS quote_currency_code, rate, 'openexchangerates.org' AS data_source, created_at, updated_at FROM exchange_rates"
 
 # Extract from source database
 psql "host=$SOURCE_DB_HOST port=$SOURCE_DB_PORT dbname=$SOURCE_DB user=$SOURCE_DB_USER password=$SOURCE_DB_PASSWORD" << EOF
@@ -13,7 +13,6 @@ psql "host=$SOURCE_DB_HOST port=$SOURCE_DB_PORT dbname=$SOURCE_DB user=$SOURCE_D
 EOF
 
 # Import to destination database
-psql "host=$POSTGRES_USER port=$POSTGRES_PORT dbname=$POSTGRES_DB user=$POSTGRES_USER password=$POSTGRES_PASSWORD" << EOF
-\COPY exchange_rates FROM 'tmp/exchange_rates.csv' WITH CSV HEADER;
-UPDATE exchange_rates SET source = 'openexchangerates.org' WHERE source IS NULL;
+psql "host=$POSTGRES_HOST port=$POSTGRES_PORT dbname=$POSTGRES_DB user=$POSTGRES_USER password=$POSTGRES_PASSWORD" << EOF
+\COPY exchange_rates(id, as_of, base_currency_code, quote_currency_code, rate, data_source, created_at, updated_at) FROM 'tmp/exchange_rates.csv' WITH CSV HEADER;
 EOF
