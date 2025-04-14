96c882eaecd2a4a59b8745d9a1453ce66deff203
John Szymanowski <github@john.szymanow.ski>
2025-04-14 10:53:43 +0800

    Use documented pattern

--- tests/models/test_exchange_rate.py
+++ tests/models/test_exchange_rate.py
@@ -2,31 +2,29 @@ from datetime import date
 from decimal import Decimal
 
 import pytest
-from tortoise import Tortoise, connections
-from tortoise.backends.base.client import BaseDBAsyncClient
+from tortoise import Tortoise
+from tortoise.contrib.fastapi import RegisterTortoise
 
-from app.core.config import settings
 from app.core.database import TORTOISE_ORM
+from app.main import app
 from app.models.exchange_rate import ExchangeRate
 
 
-@pytest.fixture(autouse=True)  # (scope="session")
+@pytest.fixture(autouse=True)
 async def initialize_tests():
     """Initialize Tortoise ORM with the pytest-postgresql database."""
 
-    print(settings.DATABASE_URL)
-
-    await Tortoise.init(TORTOISE_ORM)
-
-    conn: BaseDBAsyncClient = connections.get("default")
-    await conn.db_create()
-
-    await Tortoise.generate_schemas()
-
-    yield
-
+    async with RegisterTortoise(
+        app=app,
+        config=TORTOISE_ORM,
+        generate_schemas=True,
+        _create_db=True,
+    ):
+        # db connected
+        yield
+        # app teardown
+    # db connections closed
     await Tortoise._drop_databases()
-    await connections.close_all()
 
 
 @pytest.mark.asyncio
