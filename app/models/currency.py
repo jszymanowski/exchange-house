99443bd1357c23233be25294a83a01e77f929ec5
John Szymanowski <github@john.szymanow.ski>
2025-04-15 17:10:57 +0800

    Add helper methods to Currency

--- app/models/currency.py
+++ app/models/currency.py
@@ -2,4 +2,21 @@ from pydantic_extra_types.currency_code import Currency as BaseCurrency
 
 
 class Currency(BaseCurrency):
-    pass
+    def is_valid(self) -> bool:
+        return self.iso_code in Currency.allowed_currencies
+
+    def __eq__(self, other):
+        if isinstance(other, Currency):
+            return self.iso_code == other.iso_code
+
+        elif isinstance(other, BaseCurrency):
+            return self.iso_code == other.upper()
+
+        elif isinstance(other, str):
+            return self.iso_code == other
+
+        return NotImplemented
+
+    @property
+    def iso_code(self) -> str:
+        return self.upper()
