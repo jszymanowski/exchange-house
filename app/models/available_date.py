a2c02ec814bcfe4c217a594c1064df2e9146d4ce
John Szymanowski <github@john.szymanow.ski>
2025-04-15 13:24:39 +0800

    Lint

--- app/models/available_date.py
+++ app/models/available_date.py
@@ -7,14 +7,11 @@ class AvailableDate(date):
     """A date in the past or today."""
 
     @classmethod
-    def __get_pydantic_core_schema__(cls, _source, handler):  # type: ignore
+    def __get_pydantic_core_schema__(cls, _source, handler) -> core_schema.CoreSchema:
         return core_schema.no_info_after_validator_function(cls.validate, handler(date))
 
     @classmethod
     def validate(cls, value: date) -> date:
-        if not isinstance(value, date):
-            raise TypeError("Expected a date")
-
         if value > date.today():
             raise PydanticCustomError("date_not_in_past_or_today", "Date must be in the past or today")
 
