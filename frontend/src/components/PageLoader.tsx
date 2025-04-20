124ec5d279ea46b294017c06f8223a3baa6db32e
John Szymanowski <github@john.szymanow.ski>
2025-04-20 22:08:02 +0800

    Add useQuery

---
+++ frontend/src/components/PageLoader.tsx
@@ -0,0 +1,26 @@
+import { CircleDollarSign } from "lucide-react";
+
+import { Flex, Heading } from "@jszymanowski/breeze-primitives";
+
+interface Props {
+  message?: string;
+}
+
+export default function PageLoader({ message }: Props) {
+  return (
+    <Flex align="center" justify="center" className="h-full w-full">
+      <Flex direction="col" justify="center">
+        <Flex align="center" justify="center" gapX="4" className="my-6">
+          <CircleDollarSign size="128" className="animate-bounce text-info" />
+        </Flex>
+        <Flex direction="col" justify="center" className="max-w-[500px]">
+          {message ? (
+            <Heading level="4" align="center" weight="normal">
+              {message}
+            </Heading>
+          ) : null}
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+}
