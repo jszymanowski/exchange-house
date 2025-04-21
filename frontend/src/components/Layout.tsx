cc06684cc52259ae5bbd8d54267b11d77a795fe4
John Szymanowski <github@john.szymanow.ski>
2025-04-21 16:55:27 +0800

    Add missing component colors (#66)

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import Container from "@/components/Container";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <Container className="h-full">{children}</Container>;
+  return <Container className="mx-auto h-full">{children}</Container>;
 }
