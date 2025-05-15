a7201831a41b70d8b1bff31ebbe5adafa9cd4d6a
John Szymanowski <github@john.szymanow.ski>
2025-05-15 12:46:41 +0800

    Add footer

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,5 +1,14 @@
-import { Container } from "@still-forest/canopy";
+import { Container, Footer, Text } from "@still-forest/canopy";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <Container className="mx-auto h-dvh">{children}</Container>;
+  return (
+    <Container className="mx-auto h-dvh">
+      {children}
+      <Footer>
+        <Text variant="muted" size="sm">
+          © 2025 Still Forest LLC.
+        </Text>
+      </Footer>
+    </Container>
+  );
 }
