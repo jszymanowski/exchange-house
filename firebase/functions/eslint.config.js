ef4e68eae47b7a84a371304f25a461932ca6fc0e
John Szymanowski <github@john.szymanow.ski>
2025-05-16 22:43:28 +0800

    Add workflow to lint

---
+++ firebase/functions/eslint.config.js
@@ -0,0 +1,37 @@
+import eslintPluginImport from "eslint-plugin-import";
+import eslintPluginTs from "@typescript-eslint/eslint-plugin";
+import parserTs from "@typescript-eslint/parser";
+
+export default [
+  {
+    ignores: [
+      "/lib/**/*",
+      "/generated/**/*",
+    ],
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      ecmaVersion: 2015,
+      sourceType: "module",
+      parser: parserTs,
+      parserOptions: {
+        project: ["tsconfig.json", "tsconfig.dev.json"],
+      },
+    },
+    plugins: {
+      "@typescript-eslint": eslintPluginTs,
+      import: eslintPluginImport,
+    },
+    rules: {
+      "quotes": ["error", "double"],
+      "import/no-unresolved": "off",
+      "indent": ["error", 2],
+      "max-len": ["error", { code: 120 }],
+      ...eslintPluginTs.configs.recommended.rules,
+      ...eslintPluginImport.configs.errors.rules,
+      ...eslintPluginImport.configs.warnings.rules,
+      ...eslintPluginImport.configs.typescript?.rules || {},
+    },
+  },
+];
