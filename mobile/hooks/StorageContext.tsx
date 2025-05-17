ddca6c8a05555658510d746ac76dda8c56f38240
John Szymanowski <github@john.szymanow.ski>
2025-05-17 08:07:54 +0800

    Remove mobile code

--- mobile/hooks/StorageContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-type StorageContextType = {
-  getItem: (key: string) => Promise<string | null>;
-  setItem: (key: string, value: string) => Promise<void>;
-  subscribe: (key: string, callback: () => void) => () => void;
-};
-
-export const StorageContext = createContext<StorageContextType | null>(null);
